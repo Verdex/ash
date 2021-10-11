d114ddc92a529ac6bc17f3fd81cbe1bfde9e89da
David L. Inman <davidlevere@gmail.com>
2021-10-11 17:41:24 -0400

    create keyword parser ; create trim combinator

--- src/parsing/grammar.rs
+++ src/parsing/grammar.rs
@@ -4,6 +4,18 @@ use super::ast::Ast;
     
 use monad::compute;
 
+
+macro_rules! trim { 
+    ($p : expr) => {
+        compute!{ bind, unit => 
+            _junk_1 <- junk().zero_or_more();
+            t <- $p; 
+            _junk_2 <- junk().zero_or_more();
+            unit t.clone()
+        }
+    };
+}
+
 pub fn parse() -> Vec<Ast> {
 
     vec![]
@@ -27,16 +39,26 @@ fn bool_literal() -> Parser<Ast> {
        
     let p = compute!{ bind, unit => 
         v <- exact("true").or(exact("false"));
-        i <- not_sym_char();
+        _i <- not_sym_char();
         unit v
     };
     
     p.map(|b| Ast::Bool(b.parse::<bool>().expect("Parsed bool fails parse::<bool>()")))
 }
 
-/*fn symbol() -> Parser<String> {
+fn key(s : &'static str) -> Parser<()> {
+    let not_sym_char = || peek().when(|c| !c.is_digit(10) && *c != '_' && !c.is_alphabetic());
 
-}*/
+    trim!( compute!{bind, unit => 
+        _keyword <- exact(s);
+        _i <- not_sym_char();
+        unit () 
+    } )
+}
+
+fn junk() -> Parser<()> {
+    any().when(|c| c.is_whitespace()).map(|_x| ())
+}
 
 #[cfg(test)]
 mod test {
@@ -44,6 +66,26 @@ mod test {
     use super::super::output::Output;
     use super::super::input::Input;
     use monad::compute;
+
+    #[test]
+    fn key_combinator_should_fail_illegitimate_target() {
+        let p = key("blah");
+        let mut input = Input::new("  blahx  ");
+
+        let v = p.parse(&mut input);
+
+        assert!(matches!( v, Output::Failure(_)));
+    }
+
+    #[test]
+    fn key_combinator_should_parse_legit_target() {
+        let p = key("blah");
+        let mut input = Input::new("  blah  ");
+
+        let v = p.parse(&mut input);
+
+        assert!(matches!( v, Output::Success(_, _, _)));
+    }
 
     #[test]
     fn number_literal_should_parse_positive_value() {
